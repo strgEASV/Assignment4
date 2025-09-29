f88674a1bdc1503f65971f73a07e4f8fb61c2c7c
mirekondro1 <117477257+mirekondro1@users.noreply.github.com>
2025-09-29 11:11:34 +0200

    Exercise 2

--- src/main/java/dk/easv/assignment4/NumberConversionController.java
+++ src/main/java/dk/easv/assignment4/NumberConversionController.java
@@ -1,4 +1,31 @@
 package dk.easv.assignment4;
 
-public class NumberConversionController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
+
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class NumberConversionController implements Initializable {
+
+    private NumberConversionModel ncmodel = new NumberConversionModel();
+
+    @FXML
+    private Label label;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        TextInputDialog dialog = new TextInputDialog("");
+        dialog.setTitle("Text Input Dialog");
+        dialog.setContentText("Please enter your name:");
+
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()) {
+            String message = ncmodel.getGreetingsMessage(result.get());
+            label.setText(message);
+        }
+    }
 }
---
+++ src/main/java/dk/easv/assignment4/NumberConversionModel.java
@@ -0,0 +1,7 @@
+package dk.easv.assignment4;
+
+public class NumberConversionModel {
+    public String getGreetingsMessage(String name) {
+        return "Hello, " + name + ". Greetings from the top Java developer class in town";
+    }
+}
